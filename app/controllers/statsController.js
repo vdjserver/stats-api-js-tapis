72fe810e2429c8e596872fcede136a613d3a1b8f
Scott Christley <scott.christley@utsouthwestern.edu>
2020-03-16 12:10:47 -0500

    rearrangment count

--- app/controllers/statsController.js
+++ app/controllers/statsController.js
@@ -5,7 +5,8 @@
 // Process statistics requests
 //
 // VDJServer Community Data Portal
-// http://vdjserver.org
+// Statistics API service
+// https://vdjserver.org
 //
 // Copyright (C) 2020 The University of Texas Southwestern Medical Center
 //
@@ -25,6 +26,10 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 //
 
+// Server config
+var config = require('../config/config');
+var mongoSettings = require('../config/mongoSettings');
+
 // Controllers
 var apiResponseController = require('./apiResponseController');
 
@@ -36,11 +41,50 @@ var tapisIO = require('../vendor/tapisIO');
 
 // Node Libraries
 var Q = require('q');
+var MongoClient = require('mongodb').MongoClient;
+
+var url = 'mongodb://'
+    + mongoSettings.hostname + ':27017/' + mongoSettings.dbname;
 
 var StatisticsController = {};
 module.exports = StatisticsController;
 
-StatisticsController.RearrangementCount = function(request, response) {
+StatisticsController.RearrangementCount = async function(request, response) {
+    if (config.debug) console.log("VDJ-STATS-API INFO: StatisticsController.RearrangementCount");
+    console.log(request.body);
 
-    apiResponseController.sendSuccess('', response);
+    const client = new MongoClient(url);
+    try {
+        await client.connect();
+        if (config.debug) console.log("VDJ-STATS-API INFO: Connected successfully to mongo");
+
+        var v1airr = client.db(mongoSettings.dbname);
+        var collection = await v1airr.collection('rearrangement');
+
+        console.log(request.body.repertoires);
+        if ((!request.body.repertoires) || (request.body.repertoires.length == 0)) {
+            // empty array means total count
+            console.log('all');
+            var result = await collection.estimatedDocumentCount();
+            console.log(result);
+            apiResponseController.sendSuccess({"total_count":result}, response);
+        } else {
+            // otherwise get count for each repertoire
+            console.log('list');
+            var total_count = 0;
+            var rearrangement_count = {};
+            for (var i = 0; i < request.body.repertoires.length; ++i) {
+                console.log(request.body.repertoires[i]);
+                var result = await collection.countDocuments({"repertoire_id":request.body.repertoires[i]});
+                console.log(result);
+                rearrangement_count[request.body.repertoires[i]] = result;
+                total_count += result;
+            }
+            apiResponseController.sendSuccess({"total_count":total_count,"rearrangement_count":rearrangement_count}, response);
+        }
+    } catch (err) {
+        console.error("VDJ-STATS-API ERROR: Could not connect to database");
+        return apiResponseController.sendError("Internal Error", 500, response);
+    }
+    client.close();
 };
